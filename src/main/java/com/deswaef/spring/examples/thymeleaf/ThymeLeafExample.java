94630d69951458a726b0f2ececd8fd9088a207d5
Quinten De Swaef <quintendeswaef@gmail.com>
2014-09-09 16:30:18 +0200

    Changed package for deswaef

--- src/main/java/com/deswaef/spring/examples/thymeleaf/ThymeLeafExample.java
+++ src/main/java/com/deswaef/spring/examples/thymeleaf/ThymeLeafExample.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.qkyrie.spring.examples.thymeleaf;
+package com.deswaef.spring.examples.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- src/main/java/com/deswaef/spring/examples/thymeleaf/mvc/IndexController.java
+++ src/main/java/com/deswaef/spring/examples/thymeleaf/mvc/IndexController.java
@@ -1,19 +1,11 @@
-package com.qkyrie.spring.examples.thymeleaf.mvc;
+package com.deswaef.spring.examples.thymeleaf.mvc;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 /**
  * User: Quinten
